d03cdf985bfe9a41cfe54fa20c9e9e7d3158c39f
APTX4396 <77609544+APTX-4396@users.noreply.github.com>
2021-03-31 09:09:00 +0800

    Create 1002.cpp

---
+++ Basic Level_C++/1002.cpp
@@ -0,0 +1,55 @@
+//13分版本 三个switch语句写得太复杂了 如果不加string.h的头文件的话就会变成零分 之后再来看看怎么修改
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+	char str[101];
+	int i = 0, sum = 0, character = 0;
+	cin >> str;
+	for (i = 0; i < strlen(str); i++) {
+		character = str[i] - '0';
+		sum += character;
+	}
+	switch (sum / 100) {
+	case 0:cout << "ling "; break;
+	case 1:cout << "yi "; break;
+	case 2:cout << "er "; break;
+	case 3:cout << "san "; break;
+	case 4:cout << "si "; break;
+	case 5:cout << "wu "; break;
+	case 6:cout << "liu "; break;
+	case 7:cout << "qi "; break;
+	case 8:cout << "ba "; break;
+	case 9:cout << "jiu "; break;
+	default:break;
+	}	
+	switch ((sum-((sum/100)*100))/10) {
+	case 0:cout << "ling "; break;
+	case 1:cout << "yi "; break;
+	case 2:cout << "er "; break;
+	case 3:cout << "san "; break;
+	case 4:cout << "si "; break;
+	case 5:cout << "wu "; break;
+	case 6:cout << "liu "; break;
+	case 7:cout << "qi "; break;
+	case 8:cout << "ba "; break;
+	case 9:cout << "jiu "; break;
+	default:break;
+	}
+	switch ((sum - ((sum / 100) * 100)) % 10) {
+	case 0:cout << "ling"; break;
+	case 1:cout << "yi"; break;
+	case 2:cout << "er"; break;
+	case 3:cout << "san"; break;
+	case 4:cout << "si"; break;
+	case 5:cout << "wu"; break;
+	case 6:cout << "liu"; break;
+	case 7:cout << "qi"; break;
+	case 8:cout << "ba"; break;
+	case 9:cout << "jiu"; break;
+	default:break;
+	}
+
+	return 0;
+}
